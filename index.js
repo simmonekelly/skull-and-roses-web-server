4bcf9a3a37c48ac715001c85e9d3d57008823022
Simmone Kelly <simmone.kelly@onepeloton.com>
2024-09-10 17:52:02 -0400

    helper find functions, user and room structure, new events for challenge/card guessing

--- index.js
+++ index.js
@@ -24,6 +24,33 @@ const io = new Server(server, {
 });
 
 const rooms = [];
+const baseCards = ["rose", "rose", "skull", "rose", "rose"]
+
+const findRoom = (currentRoom) => {
+  const filteredRoom = rooms.find((room) => room.roomId === currentRoom);
+  return filteredRoom;
+};
+
+const findUser = (room, currentUser) => {
+  const filteredUser = room.players.find((player) => player.id === currentUser);
+  return filteredUser;
+};
+
+const findRoomAndUser = (currentRoom, currentUser) => {
+  const room = findRoom(currentRoom);
+
+  if (!room) {
+    return undefined;
+  }
+
+  const user = findUser(room, currentUser);
+
+  if (!user) {
+    return undefined;
+  }
+
+  return { room, user };
+};
 
 io.on("connection", (socket) => {
   console.log(`user connected: ${socket.id}`);
@@ -35,14 +62,27 @@ io.on("connection", (socket) => {
       separator: "-",
     });
 
+    const currentUser = {
+      id: socket.id,
+      cards: ["rose", "rose", "skull", "rose", "rose"],
+      matStatus: false,
+      gameControler: true,
+    };
+
     const room = {
       roomId: roomId,
-      players: [socket.id],
+      players: [currentUser],
+      stockPile: [],
+    };
+
+    const newRoom = {
+      room,
+      currentUser: currentUser,
     };
 
     rooms.push(room);
     socket.join(roomId);
-    createRoom(room);
+    createRoom(newRoom);
   });
 
   //when a user joins a game room
@@ -50,15 +90,121 @@ io.on("connection", (socket) => {
     console.log(`${socket.id} joined room ${roomToJoin}`);
     if (rooms.find((room) => room.roomId === roomToJoin)) {
       const room = rooms.find((room) => room.roomId === roomToJoin);
-      room.players.push(socket.id);
+
+      const currentUser = {
+        id: socket.id,
+        cards: ["rose", "rose", "skull", "rose", "rose"],
+        matStatus: false,
+        gameControler: false,
+      };
+
+      room.players.push(currentUser);
+
+      const joinedRoom = {
+        room,
+        currentUser,
+      };
       socket.join(room.roomId);
-      joinRoom(room);
+      joinRoom(joinedRoom);
 
       io.in(room.roomId).emit("new_user_joins", room);
     } else {
       //handle error
     }
   });
+
+  socket.on(
+    "submit_card",
+    (currentRoom, currentUser, cardData, updateCards) => {
+      console.log("update card");
+      if (rooms.find((room) => room.roomId === currentRoom)) {
+        const room = rooms.find((room) => room.roomId === currentRoom);
+        const user = room.players.find((player) => player.id === currentUser);
+
+        // console.log({room, user, cardData})
+        user.cards.splice(cardData.cardIndex, 1);
+        // console.log({user})
+        room.stockPile.push(cardData.cardText);
+
+        updateCards(user);
+
+        io.in(room.roomId).emit("update_room", room);
+      } else {
+        //handle error
+      }
+    }
+  );
+
+  socket.on("submit_guess", (currentRoom, currentUser, userGuess, callback) => {
+    const data = findRoomAndUser(currentRoom, currentUser);
+    if (data) {
+      // console.log({rooms, data, currentRoom, currentUser, userGuess, callback})
+      callback();
+
+      io.in(data.room.roomId).emit(
+        "update_countdown",
+        data.room,
+        data.user.id,
+        userGuess
+      );
+    } else {
+      console.log("error submitting guess: no room or user");
+    }
+  });
+
+  socket.on(
+    "update_mat_status",
+    (currentRoom, user, currentUser, updatedMatStatus, callback) => {
+      console.log({currentRoom, user, currentUser, updatedMatStatus, callback})
+      const data = findRoomAndUser(currentRoom, user);
+      const currentUserData = findUser(data.room, currentUser)
+
+      if (data && currentUser) {
+
+        if (updatedMatStatus) {
+  
+          //reset current user cards
+          currentUserData.cards = baseCards;
+
+          //add logic to check what current status is
+          //if true then emit event that x user has won the game
+          console.log("true");
+          if (user === currentUser) {
+            //update current user
+            currentUserData.matStatus = updatedMatStatus;
+            callback(currentUserData);
+          } else {
+
+            callback(currentUserData);
+          }
+
+          //reset all user cards
+          data.room.players.forEach((player) => (
+            player.cards = baseCards
+          ))
+          data.user.matStatus = updatedMatStatus;
+          data.room.stockPile = []
+          console.log({data})
+          io.in(data.room.roomId).emit("update_room", data.room);
+          io.in(data.room.roomId).emit("show_update_modal");
+        } else {
+          console.log("false");
+          //for now removes the last card a user has
+
+          //will need to log how many cards each user has, and rest accordingly
+          //remove 1 card from user that lost
+          data.user.cards.splice(data.user.cards.length - 1, 1);
+          if (user === currentUser) {
+            callback(data.user);
+          }
+          //emit update modal
+          io.in(data.room.roomId).emit("update_room", data.room);
+        }
+      } else {
+        console.log("cant update mat status: no room or user");
+      }
+    }
+  );
 });
 
 server.listen(8080, () => {
